bf6f1e182f5c82ef549b5db12a384a2944b9adf9
Gary Wilber <Spacetech326@gmail.com>
2018-10-18 01:35:30 -0700

    Development branch update

--- out/ui/inspect/Entity.d.ts
+++ out/ui/inspect/Entity.d.ts
@@ -22,7 +22,7 @@ export default class EntityInformation extends InspectInformationSection {
     setTab(entity: number): this;
     update(position: IVector2, tile: ITile): void;
     getIndex(entity: ICreature | INPC | IPlayer): number;
-    getEntity(index: number): ICreature | INPC | IPlayer;
+    getEntity(index: number): import("../../../mod-reference/definitions/entity/IEntity").Entity;
     logUpdate(): void;
     private initializeStats;
     private onStatChange;
