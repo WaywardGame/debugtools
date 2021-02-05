e9388eef1e534f5aa0338879a055a5420ffad920
Gary Wilber <spacetech326@gmail.com>
2021-02-05 01:41:49 -0800

    Update for 2.10.0-beta.dev.20210205.1

--- out/action/Remove.d.ts
+++ out/action/Remove.d.ts
@@ -1,5 +1,5 @@
 import { Action } from "entity/action/Action";
 import { ActionArgument } from "entity/action/IAction";
 import Player from "entity/player/Player";
-declare const _default: Action<[[ActionArgument.Entity, ActionArgument.Doodad, ActionArgument.Corpse, ActionArgument.TileEvent, ActionArgument.Item]], Player, void, [import("../../node_modules/@wayward/types/definitions/entity/Entity").default | import("../../node_modules/@wayward/types/definitions/item/Item").default | import("../../node_modules/@wayward/types/definitions/doodad/Doodad").default | import("../../node_modules/@wayward/types/definitions/entity/creature/corpse/Corpse").default | import("../../node_modules/@wayward/types/definitions/tile/TileEvent").default]>;
+declare const _default: Action<[[ActionArgument.Entity, ActionArgument.Doodad, ActionArgument.Corpse, ActionArgument.TileEvent, ActionArgument.Item]], Player, void, [import("../../node_modules/@wayward/types/definitions/doodad/Doodad").default | import("../../node_modules/@wayward/types/definitions/item/Item").default | import("../../node_modules/@wayward/types/definitions/entity/Entity").default | import("../../node_modules/@wayward/types/definitions/tile/TileEvent").default | import("../../node_modules/@wayward/types/definitions/entity/creature/corpse/Corpse").default]>;
 export default _default;
