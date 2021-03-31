4e9a05cdfa6d12a6c69d3fcce529da3b027e8026
Gary Wilber <spacetech326@gmail.com>
2021-03-31 02:34:14 -0700

    Update for 2.10.0-beta.dev.20210331.1

--- out/action/Heal.d.ts
+++ out/action/Heal.d.ts
@@ -1,4 +1,4 @@
 import { Action } from "game/entity/action/Action";
 import { ActionArgument } from "game/entity/action/IAction";
-declare const _default: Action<[[ActionArgument.Entity, ActionArgument.Corpse]], import("../../node_modules/@wayward/types/definitions/game/entity/player/Player").default, void, [import("../../node_modules/@wayward/types/definitions/game/entity/Entity").default | import("../../node_modules/@wayward/types/definitions/game/entity/creature/corpse/Corpse").default]>;
+declare const _default: Action<[[ActionArgument.Entity, ActionArgument.Corpse]], import("../../node_modules/@wayward/types/definitions/game/entity/player/Player").default, void, [import("../../node_modules/@wayward/types/definitions/game/entity/creature/corpse/Corpse").default | import("../../node_modules/@wayward/types/definitions/game/entity/Entity").default]>;
 export default _default;
