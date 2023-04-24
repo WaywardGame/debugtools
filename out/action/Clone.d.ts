2c8e6ef916c26fa40ef97815241a28af23fc188c
Spacetech <spacetech326@gmail.com>
2023-04-24 05:38:46 -0700

    Update for 2.13.0-beta.dev.20230424.1

--- out/action/Clone.d.ts
+++ out/action/Clone.d.ts
@@ -2,5 +2,5 @@ import Doodad from "game/doodad/Doodad";
 import { Action } from "game/entity/action/Action";
 import { ActionArgument } from "game/entity/action/IAction";
 import Tile from "game/tile/Tile";
-declare const _default: Action<[[ActionArgument.Entity, ActionArgument.Doodad], ActionArgument.Tile], import("../../node_modules/@wayward/types/definitions/game/game/entity/player/Player").default, void, import("game/entity/action/IAction").IActionUsable, [import("../../node_modules/@wayward/types/definitions/game/game/entity/Entity").default<unknown, number, unknown, unknown> | Doodad, Tile]>;
+declare const _default: Action<[[ActionArgument.Entity, ActionArgument.Doodad], ActionArgument.Tile], import("../../node_modules/@wayward/types/definitions/game/game/entity/player/Player").default, void, import("game/entity/action/IAction").IActionUsable, [Doodad | import("../../node_modules/@wayward/types/definitions/game/game/entity/Entity").default<unknown, number, unknown, unknown>, Tile]>;
 export default _default;
