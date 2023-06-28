ca8929f10353d9045bcd8ec2ac3379e13fe179d2
Spacetech <spacetech326@gmail.com>
2023-06-28 05:04:16 -0700

    Update for 2.13.3-beta.dev.20230628.1

--- out/ui/DebugToolsPrompts.js
+++ out/ui/DebugToolsPrompts.js
@@ -1,3 +1,13 @@
+/*!
+ * Copyright 2011-2023 Unlok
+ * https://www.unlok.ca
+ *
+ * Credits & Thanks:
+ * https://www.unlok.ca/credits-thanks/
+ *
+ * Wayward is a copyrighted and licensed work. Modification and/or distribution of any source files is prohibited. If you wish to modify the game in any way, please refer to the modding guide:
+ * https://github.com/WaywardGame/types/wiki
+ */
 var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
     var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
     if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
@@ -17,4 +27,4 @@ define(["require", "exports", "mod/Mod", "mod/ModRegistry"], function (require, 
         ModRegistry_1.default.prompt("ReplacePlayerData", (type, prompt) => prompt.confirm(type))
     ], DebugToolsPrompts.prototype, "promptReplacePlayerData", void 0);
 });
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiRGVidWdUb29sc1Byb21wdHMuanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyIuLi8uLi9zcmMvdWkvRGVidWdUb29sc1Byb21wdHMudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6Ijs7Ozs7Ozs7O0lBTUEsTUFBcUIsaUJBQWlCO0tBUXJDO0lBUkQsb0NBUUM7SUFMZ0I7UUFEZixhQUFHLENBQUMsUUFBUSxDQUFhLGFBQWEsQ0FBQzswREFDQTtJQUd4QjtRQURmLHFCQUFRLENBQUMsTUFBTSxDQUFDLG1CQUFtQixFQUFFLENBQUMsSUFBSSxFQUFFLE1BQU0sRUFBRSxFQUFFLENBQUMsTUFBTSxDQUFDLE9BQU8sQ0FBNkIsSUFBSSxDQUFDLENBQUM7c0VBQ1YifQ==
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiRGVidWdUb29sc1Byb21wdHMuanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyIuLi8uLi9zcmMvdWkvRGVidWdUb29sc1Byb21wdHMudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IkFBQUE7Ozs7Ozs7OztHQVNHOzs7Ozs7Ozs7O0lBUUgsTUFBcUIsaUJBQWlCO0tBUXJDO0lBUkQsb0NBUUM7SUFMZ0I7UUFEZixhQUFHLENBQUMsUUFBUSxDQUFhLGFBQWEsQ0FBQzswREFDQTtJQUd4QjtRQURmLHFCQUFRLENBQUMsTUFBTSxDQUFDLG1CQUFtQixFQUFFLENBQUMsSUFBSSxFQUFFLE1BQU0sRUFBRSxFQUFFLENBQUMsTUFBTSxDQUFDLE9BQU8sQ0FBNkIsSUFBSSxDQUFDLENBQUM7c0VBQ1YifQ==
