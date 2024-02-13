947beb995f39a5078d2eb4c12d248305719fd203
Spacetech <spacetech326@gmail.com>
2024-02-13 04:06:23 -0800

    Update for 2.14.0-beta.dev.20240213.1

--- out/ui/component/MagicalPropertiesEditor.d.ts
+++ out/ui/component/MagicalPropertiesEditor.d.ts
@@ -1,3 +1,13 @@
+/*!
+ * Copyright 2011-2023 Unlok
+ * https://www.unlok.ca
+ *
+ * Credits & Thanks:
+ * https://www.unlok.ca/credits-thanks/
+ *
+ * Wayward is a copyrighted and licensed work. Modification and/or distribution of any source files is prohibited. If you wish to modify the game in any way, please refer to the modding guide:
+ * https://github.com/WaywardGame/types/wiki
+ */
 import Doodad from "@wayward/game/game/doodad/Doodad";
 import Item from "@wayward/game/game/item/Item";
 import SingletonEditor from "./SingletonEditor";
